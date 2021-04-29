12067a9299192cf4cefe1e2e55842d7c474de3ce
yuxuchen <chenyuxuan0930@gmail.com>
2021-04-30 03:53:45 +1000

    new change

--- react-redux/TodoList.js
+++ react-redux/TodoList.js
@@ -1,42 +1,67 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux';
 
-class TodoList extends Component{
+//此时TodoList是一个UI组件，并不包含任何的函数逻辑，同时他只有一个render函数
+//可以把它写为无状态组件
+//此时TodoList是一个UI以及无状态组件
 
-    render(){
-        return(
+const TodoList =(props)=>{
+    //结构赋值使得代码更加精简化，const {inputValue}=this.props 等价于 this.props.inputValue
+    const {inputValue,changeInputValue,handleClick,handleDelete,list} =props;
+    
+    return(
+        <div>
             <div>
-                <div>
-                    <input value={this.props.inputValue} onChange={this.props.changeInputValue}/>
-                    <button>submit</button>
-                </div>
-                <ul>
-                    <li>dell</li>
-                </ul>
+                <input value={inputValue} onChange={changeInputValue}/>
+                <button onClick={handleClick}>submit</button>
             </div>
-        )
+            <ul>
+                {
+                    list.map((item,index)=>{
+                        return <li onClick={handleDelete} key={index}>{item}</li>
+                    })
+                }
+            </ul>
+        </div>
+
+    )
+}
+
+
+
+const mapStateToProps=(state)=>{
+    return{
+        inputValue:state.inputValue,
+        list:state.list
     }
 }
 //connect意思是让TodoList和store做连接
-//mapStateToProps把store里面的数据映射给组件作为props，state代表store的数据
+//mapStateToProps把store里面的数据映射给组件作为props，state代表store的数据,代表了TodoList和store连接的规则
 //所以store里面inputValue的值直接映射给todolist里的inputValue的值，作为props储存起来
-const mapStateToProps=(state)=>{
-    return{
-        inputValue:state.inputValue
-    }
-}
-//store.dispatch映射到props,所以可以通过this.props来调用dispatch方法
 const mapDispatchToProps=(dispatch)=>{
-    return{
-        changeInputValue(e){
-            const action={
-                type:'change_input_value',
-                value:e.target.value
-            }
-            dispatch(action);
+    return{changeInputValue(e){
+        const action={
+            type:'change_input_value',
+            value:e.target.value
         }
-
+        dispatch(action)
+    },
+    handleClick(){
+        const action={
+            type:'add_item'
+        }
+        dispatch(action)
+    },
+    handleDelete(){
+        const action={
+            type:'delete_item'
+        }
+        dispatch(action)
     }
 }
+}
+
+//store.dispatch映射到props,所以可以通过this.props来调用dispatch方法
 //前两个参数代表连接的规则，最后一个参数代表需要连接的组件名称
-export default connect(mapStateToProps,mapDispatchToProps)(TodoList);
+export default connect(mapStateToProps,mapDispatchToProps)(TodoList)
+//TodoList是个UI组件，当connect把UI组件和逻辑组件相结合返回的时候，返回的是一个容器组件
--- react-redux/store/reducer.js
+++ react-redux/store/reducer.js
@@ -1,12 +1,24 @@
 const defaultState={
-    inputValue:'write here',
+    inputValue:'',
     list:[]
 }
-export default (state=defaultState,action)=>{
-    if (action.type === 'change_input_value'){
-        const newState=JSON.parse(JSON.stringify(state))
-        newState.inputValue=action.value;
+
+export default(state=defaultState, action)=>{
+    if(action.type==='change_input_value'){
+        const newState=JSON.parse(JSON.stringify(state));
+        newState.inputValue=action.value  
+        return newState
+    }
+    if(action.type==='add_item'){
+        const newState=JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue='';
         return newState;
+    }
+    if(action.type==='delete_item'){
+        const newState=JSON.parse(JSON.stringify(state));
+        newState.list.splice(action.index,1)
+        return newState
     }
     return state;
 }
